eb8637a1ff5a89e8f6456a89f8a92264db83ed15
Santosh Kumar <sntshkmr60@gmail.com>
2022-04-12 19:34:14 +0530

    Add Artist entity

---
+++ MusicPlayer/src/entities/Artist.java
@@ -0,0 +1,32 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Artist {
+    private int artistId;
+    private String firstName;
+    private String lastName;
+    private List<Song> releasedSongs;
+    private static int counter = 1;
+
+    public Artist(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.artistId = counter;
+        counter++;
+        releasedSongs = new ArrayList<>();
+    }
+
+    public int getArtistId() {
+        return artistId;
+    }
+
+    public String getName() {
+        return firstName + " " + lastName;
+    }
+
+    public List<Song> getReleasedSongs() {
+        return releasedSongs;
+    }
+}
