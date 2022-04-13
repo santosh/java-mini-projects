94eebde96db3a2c01a0d9e23aec537a381b6b932
Santosh Kumar <sntshkmr60@gmail.com>
2022-04-13 08:30:53 +0530

    Implement GetTop10Songs and getTop10SongsForUser

--- MusicPlayer/src/application/MusicLibrary.java
+++ MusicPlayer/src/application/MusicLibrary.java
@@ -48,4 +48,38 @@ public class MusicLibrary {
 
         }
 
+    private List<Map.Entry<Integer, Integer>> getTopNByValue(Map<Integer, Integer> map, int n) {
+        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
+            @Override
+            public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
+                return o2.getValue() - o1.getValue();
+            }
+        });
+        return list.subList(0, Math.min(n, list.size()));
+    }
+
+    public void getTop10Songs() {
+        List<Map.Entry<Integer, Integer>> top10Songs = getTopNByValue(songPlayCounts, 10);
+
+        for (int i = 0; i < top10Songs.size(); i++) {
+            System.out.println("Song : " + songs.get(top10Songs.get(i).getKey()).getTitle() + ", played: " +
+                    top10Songs.get(i).getValue() + " times");
+        }
+    }
+
+    public void getTop10SongsForUser(int userId) throws Exception {
+        if (!users.containsKey(userId)) {
+            throw new Exception("User is invalid");
+        }
+        Map<Integer, Integer> usersSongs = userPlayHistory.get(userId);
+
+        List<Map.Entry<Integer, Integer>> top10Songs = getTopNByValue(usersSongs, 10);
+
+        for (int i = 0; i < top10Songs.size(); i++) {
+            System.out.println("Song : " + songs.get(top10Songs.get(i).getKey()).getTitle() + ", played: " +
+                    top10Songs.get(i).getValue() + " times");
+        }
+    }
+
 }
