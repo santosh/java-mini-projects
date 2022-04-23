7639ab5175c4a3fc2a542e12f869ed29323ec194
Santosh Kumar <sntshkmr60@gmail.com>
2022-04-23 19:28:28 +0530

    Register new user

--- TwitterClone/src/TwitterClone.java
+++ TwitterClone/src/TwitterClone.java
@@ -1,10 +1,37 @@
+import java.util.Objects;
+
 public class TwitterClone {
     private User[] allUsers;
     private Tweet[] allTweets;
 
-    private int maxUserId;
+    private int maxUserId = 0;
 
     public TwitterClone() {
 
+    }
+
+    public void createNewUser(String userName) {
+//        TODO: Check if userID already exists
+//        TODO: Create a new User
+//        TODO: Add to allUsers array
+        if (userName.equals("")) {
+            System.out.println("userName cannot be blank!");
+        } else if (getUser(userName) != null) {
+            System.out.println("This user already exists!");
+        } else {
+            User user = new User(userName);
+            allUsers[maxUserId] = user;
+            maxUserId++;
+
+        }
+    }
+
+    private User getUser(String username) {
+        for (User u : allUsers) {
+            if (Objects.equals(u.userName, username)) {
+                return u;
+            }
+        }
+        return null;
     }
 }
