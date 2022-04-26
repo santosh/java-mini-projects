aff02c1ff6823ee8dc2bb72332387ecae91146fb
Santosh Kumar <sntshkmr60@gmail.com>
2022-04-26 10:59:30 +0530

    Enforce User and Tweet limit

--- TwitterClone/src/TwitterClone.java
+++ TwitterClone/src/TwitterClone.java
@@ -4,24 +4,36 @@ public class TwitterClone {
     private User[] allUsers;
     private Tweet[] allTweets;
 
-    private int maxUserId = 0;
+    private int maxUserId;
+    private int maxTweetId;
 
     public TwitterClone() {
+        this.allTweets = new Tweet[1000];
+        this.allUsers = new User[1000];
 
+        this.maxUserId = -1;
+        this.maxTweetId = -1;
     }
 
     public void createNewUser(String userName) {
-//        TODO: Check if userID already exists
-//        TODO: Create a new User
-//        TODO: Add to allUsers array
-        if (userName.equals("")) {
+        /*
+        TODO: Assign a unique id
+        TODO: Check if limits have exceeded
+        TODO: Check if userID already exists
+        TODO: Create a new User object
+        TODO: Add to allUsers array
+         */
+        if (maxUserId == 999) {
+            System.out.println("Sorry, limit exceeded. Can't onboard a new user now..");
+        } else if (userName.equals("")) {
             System.out.println("userName cannot be blank!");
         } else if (getUser(userName) != null) {
             System.out.println("This user already exists!");
         } else {
-            User user = new User(userName);
-            allUsers[maxUserId] = user;
             maxUserId++;
+            User user = new User(maxUserId, userName);
+            allUsers[maxUserId] = user;
+
 
         }
     }
